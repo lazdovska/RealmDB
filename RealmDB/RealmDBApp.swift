bfb64df7f868012005f4bf82080f736546d396d8
Kristine Lsx <lazdovska.kristine@gmail.com>
2021-09-02 10:21:00 +0300

    Initial Commit

---
+++ RealmDB/RealmDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmDBApp.swift
+//  RealmDB
+//
+//  Created by kristine.lazdovska on 02/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct RealmDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
